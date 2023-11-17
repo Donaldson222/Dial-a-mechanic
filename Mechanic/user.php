d19741a19b4be9875b4e19416ee7ac00f6ec2736
Donaldson222 <148865167+Donaldson222@users.noreply.github.com>
2023-11-17 12:02:09 +0300

    Update user.php

--- Mechanic/user.php
+++ Mechanic/user.php
@@ -12,8 +12,17 @@
     <meta charset="UTF-8">
     <title>User Page</title>
     <link rel="stylesheet" href="style4.css">
+     <meta http-equiv="refresh"
+          content="2;  
+                url=http://localhost:3000/booking.html">
     </head>
     <body>
+        You will be redirected to the Booking Page in a moment. 
+    <br>  
+    If you are not redirected automatically,  
+    <a href="http://localhost:3000/booking.html"> 
+      click here 
+    </a>. 
         <div class="container">
         <div class="content">
          <h3>Hi <span>Customer</span></h3>
