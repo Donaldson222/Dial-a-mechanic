5227046dc27e8d94f9811ef0ecae0f2f3505294f
Donaldson222 <148865167+Donaldson222@users.noreply.github.com>
2023-11-21 22:58:42 +0300

    Update admin.php

--- Mechanic/admin.php
+++ Mechanic/admin.php
@@ -1,28 +1,93 @@
-<?php
-    @include 'config.php';
-    session_start();
-    if(isset($_SESSION['admin_name'])){
-        header('location:login.php');
-    }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initail-scale=1.0">
-    <meta charset="UTF-8">
-    <title>Admin Page</title>
-    <link rel="stylesheet" href="style4.css">
-    </head>
-    <body>
-        <div class="container">
-        <div class="content">
-         <h3>Hi <span>Admin</span></h3>
-         <h1>Welcome<span></span></h1>
-        
-         <a href="login.php" class="btn">Login</a>
-         <a href="register.php" class="btn">Register</a>
-         <a href="logout.php" class="btn">Logout</a>
-         </div>
-        </div>
-    </body>
-</html>
+<?php   
+ include 'config.php';  
+ $query = "select * from admin_db";  
+ $run = mysqli_query($conn,$query);  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+ <head>  
+
+      <meta charset="utf-8">  
+      <title>Delete Data From Database in PHP</title>  
+      <link rel="stylesheet" type="text/css" href="style.css">  
+ </head>  
+ <body>  
+ <header></header>  
+ <table border="1" cellspacing="0">
+      <tr class="heading">    
+           <th>ID</th>  
+           <th>Name</th>  
+           <th>Email</th>  
+           <th>Password</th>  
+           <th>User Type</th>
+      </tr>  
+      <?php   
+      $i=1;  
+           if ($num = mysqli_num_rows($run)>0) {  
+                while ($result = mysqli_fetch_assoc($run)) {  
+                     echo "  
+                          <tr class='data'>  
+                          <td>".$i++."</td>  
+                               <td>".$result['id']."</td>  
+                               <td>".$result['name']."</td>  
+                               <td>".$result['email']."</td>  
+                               <td>".$result['password']."</td>  
+                               <td>".$result['user_type']."</td>   
+                               <td><a href='delete.php?id=".$result['id']."' id='btn'>Delete</a></td>  
+                          </tr>  
+                     ";  
+                }  
+           }  
+      ?>  
+ </table>  
+ <style>
+    *{  
+      padding: 0;  
+      margin: 0;  
+      box-sizing: border-box;  
+ }  
+ body{  
+      width: 100%;  
+      height: 100vh;  
+      background-color: #34495e;  
+      position: relative;  
+      font-family: 'verdana',sans-serif;  
+ }  
+ header{  
+      width: 100%;  
+      height: 80px;  
+      background-color: #2c3e50;  
+ }  
+ table{  
+      width: 75%;  
+      position: absolute;  
+      top: 50%;  
+      left: 50%;  
+      transform: translate(-50%,-50%);  
+ }  
+ .heading{  
+      background-color: #FFFF;  
+ }  
+ .heading th{  
+      padding: 10px 0;  
+ }  
+ .data{  
+      text-align: center;  
+      color: #FFFF;  
+ }  
+ .data td{  
+      padding: 15px 0;  
+ }  
+ #btn{  
+      text-decoration: none;  
+      color: #FFF;  
+      background-color: #e74c3c;  
+      padding: 5px 20px;  
+      border-radius: 3px;  
+ }  
+ #btn:hover{  
+      background-color: #c0392b;  
+ }  
+ </style>
+ </body>  
+ </html>  
